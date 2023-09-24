a64e1ddf90c41d2c959d0cd705fb0a405637e341
Rafael Costa <rafael.costab@hotmail.com>
2023-09-23 22:08:22 -0300

    2.9 creating the project via the STS wizard

---
+++ src/main/java/com/rafaelcostab/employeecontrol/EmployeeControlApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelcostab.employeecontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeControlApplication.class, args);
+	}
+
+}
